c82d936519ce555f8df63f36cf5df6a49ec73eb8
Oliverhiggins93 <oliverhiggins93@hotmail.co.uk>
2020-07-14 11:02:52 +0100

    Add dependencies

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Set constraints for the video stream
-var constraints = { video: { facingMode: "user" }, audio: false };
+var constraints = { video: { facingMode: "environment" }, audio: false };
 var textCounter = 0;
 //var miniButtons = document.getElementById("miniButtons");
 //miniButtons.style.display = "none"
